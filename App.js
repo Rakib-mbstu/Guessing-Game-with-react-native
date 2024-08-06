f269a999c1e6a19bf6f16559a5188fe3feed7eec
rakib-mbstu <mirrakibulislam4@gmail.com>
2024-08-06 16:38:00 +0600

    Bg image added

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.rootContainer}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootContainer}>
+      <ImageBackground
+        source={require('./assets/images/bg.jpg')}
+        resizeMode="cover"
+        style={styles.rootContainer}
+        imageStyle={styles.bgImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    backgroundColor: '#FFFFFF'
   },
+  bgImage: {
+    opacity: 0.25,
+  }
 });
