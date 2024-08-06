45eb89cdef3823464aa4277a4c77533e8756b2d2
rakib-mbstu <mirrakibulislam4@gmail.com>
2024-08-06 14:29:12 +0600

    opacity added

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
+    function pressHandler() {
+        console.log("Pressed");
+    }
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable onPress={pressHandler}
+                style={({ pressed }) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}>
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#7206ef',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75,
+    }
 
+})
 export default PrimaryButton;
